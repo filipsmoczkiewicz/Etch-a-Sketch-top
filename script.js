195fa4b5a37a421c4480d63bab1990c7db8e22c5
Filip Smoczkiewicz <106594883+filipsmoczkiewicz@users.noreply.github.com>
2023-05-11 19:11:39 +0200

    The user can now declare the desired amount of squares within the grid.

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
 const container = document.querySelector(".container");
+const newGridButton = document.querySelector("#new-grid");
 
-// create 16x16 grid
-for (let i = 0; i < 256; i++) {
-  const square = document.createElement("div");
-  square.classList.add("square");
-  container.appendChild(square);
+// function to create the grid
+function createGrid(numSquares) {
+  // calculate the size of each square
+  const squareSize = 480 / numSquares;
 
-  // add event listener to change color on mouseover
-  square.addEventListener("mouseover", () => {
-    square.style.backgroundColor = "black";
-  });
+  // remove all existing squares from the container
+  container.innerHTML = '';
+
+  // create the new grid
+  for (let i = 0; i < numSquares * numSquares; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.style.width = `${squareSize}px`;
+    square.style.height = `${squareSize}px`;
+    container.appendChild(square);
+
+    // add event listener to change color on mouseover
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = "black";
+    });
+  }
 }
+
+// create initial grid with 16x16 squares
+createGrid(16);
+
+// add event listener to the "New Grid" button
+newGridButton.addEventListener("click", () => {
+  // prompt the user for the number of squares they want in the grid
+  const numSquares = prompt("How many squares do you want in the grid?");
+
+  // if user clicks "Cancel" or inputs an invalid number, do nothing
+  if (numSquares === null || isNaN(numSquares)) {
+    return;
+  }
+
+  // create the new grid with the specified number of squares
+  createGrid(parseInt(numSquares));
+});
